69ca116822b6a7f58ff24c974bf3570c6f8bc624
malek <malek.chalbi@esprit.tn>
2024-10-25 16:18:22 +0100

    testttt

--- src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
@@ -1,7 +1,6 @@
 package tn.esprit.spring.kaddem.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +8,7 @@ import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,9 +28,11 @@ public class DepartementServiceImpl implements IDepartementService{
 		return departementRepository.save(d);
 	}
 
-	public  Departement retrieveDepartement (Integer idDepart){
-		return departementRepository.findById(idDepart).get();
+	public Departement retrieveDepartement(Integer idDepart) {
+		Optional<Departement> departement = departementRepository.findById(idDepart);
+        return departement.orElse(null);
 	}
+
 	public  void deleteDepartement(Integer idDepartement){
 		Departement d=retrieveDepartement(idDepartement);
 		departementRepository.delete(d);
